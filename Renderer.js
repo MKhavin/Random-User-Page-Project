62a38e81ca7d184d4f25501ad739dabe66e6911e
MichaelK <havinmichael@gmail.com>
2023-07-17 19:17:38 +0300

    Implement first version of Renderer + Implement Handlebars templates

--- Renderer.js
+++ Renderer.js
@@ -1,4 +1,19 @@
-
 class Renderer {
+  renderView(data) {
+    this.#renderData(".user-container", "#user-info-template", data.userData);
+    this.#renderData(".quote-container", "#quote-template", data);
+    this.#renderData(".friends-container", "#users-friends-template", data);
+    this.#renderData(".meat-container", "#meat-template", data);
+    this.#renderData(".pokemon-container", "#pokemon-template", data.favPokemon);
+  }
 
+  #renderData(parentId, templateId, data) {
+    const parentElement = $(parentId);
+    parentElement.empty()
+
+    const template = Handlebars.compile($(templateId).html());
+    const view = template(data);
+
+    parentElement.append(view);
+  }
 }
