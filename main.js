b94a837ff01947789f071590b78dc43698a547eb
MichaelK <havinmichael@gmail.com>
2023-07-19 11:21:01 +0300

    Implement save/load user data to/from local machine

--- main.js
+++ main.js
@@ -28,6 +28,8 @@ class Controller {
 
     #setEventListeners() {
         $("#generate-user-button").click(this.#loadUserData.bind(this))
+        $("#save-user-data-button").click(this.#saveUserData.bind(this))
+        $("#load-user-data-button").click(this.#loadUserDataFromFile.bind(this))
     }
 
     #setHandlebarHelpers() {
@@ -36,6 +38,38 @@ class Controller {
 
             return lowerCasedValue.slice(0, 1).toUpperCase() + lowerCasedValue.slice(1)
         })
+    }
+
+    #getUserDataFileType() {
+        return [
+            {
+                description: "JSON file",
+                accept: { "application/json": [".json"] },
+            },
+        ]
+    }
+
+    #saveUserData() {
+        const options = {
+            suggestedName: "UserData",
+            types: this.#getUserDataFileType()
+        }
+
+        window.showSaveFilePicker(options).then(handler => {
+            this.#apiManager.saveUserData(handler)
+        });
+    }
+
+    #loadUserDataFromFile() {
+        const options = {
+            types: this.#getUserDataFileType(),
+            excludeAcceptAllOption: true,
+            multiple: false
+        }
+
+        window.showOpenFilePicker(options)
+            .then(handler => this.#apiManager.loadUserDataFromFile(handler))
+            .then(result => this.#renderer.renderView(result))
     }
 }
 
